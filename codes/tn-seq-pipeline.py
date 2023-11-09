780182486bead802703566d4708f921ef9657aea
Luka Lmelias <lmeliasluke@gmail.com>
2023-11-10 00:39:49 +0100

    using config to get tool paramerts instaed of argparse

--- codes/tn-seq-pipeline.py
+++ codes/tn-seq-pipeline.py
@@ -2,7 +2,18 @@ import os
 import subprocess
 import argparse
 import re
+import json
 
+def get_args(config_file_path):    
+    """
+    reads the json config file to extract tpp arguments
+
+    returns a dict of args
+    """
+    # Load configuration from the JSON file
+    with open(config_file_path, "r") as config_file:
+        config = json.load(config_file)
+    return config
 
 def find_sequence_files(raw_data_directory):
     """
@@ -69,61 +80,76 @@ def run_tpp(raw_data_directory, bwa_path, reference, read1_primer,
         elif re.search(R2_pattern, seq):
             primer = read2_primer
 
-        
-        
+        seq_base_name = seq.split("/")[-1].split("_R")[0]
+        outputfile = os.path.join(tpp_output_dir, seq_base_name)
 
         cmd = f"python src/tpp.py -bwa {bwa_path} \
         -ref {reference} -reads1 {seq} \
-        -output {tpp_output_dir} -protocol {protocol} \
+        -output {outputfile} -protocol {protocol} \
         -replicon-ids {replicon_ids} -primer {primer}"
 
-        print(cmd,"\n")
+        print("\n",cmd,"\n")
+
+        #Run the command and capture the output
+        output = subprocess.check_output(cmd, shell=True)
+
+                
+        # Print the output
+        print("\n",output.decode('utf-8'),"\n")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Runs tpp pipeline")
     
     #raw data directory
-    parser.add_argument("-d", "--directory", help="The directory to search for sequence files")
+    parser.add_argument("-c", "--config_file_path", help="The directory to search for sequence files")
 
-    #path to reference genome
-    parser.add_argument("-r",  "--reference", help="path to reference genome")
+    # #path to reference genome
+    # parser.add_argument("-r",  "--reference", help="path to reference genome")
 
-    # path to bwa excutable
-    parser.add_argument("-b", "--bwa_path", help="path to bwa excutable")
+    # # path to bwa excutable
+    # parser.add_argument("-b", "--bwa_path", help="path to bwa excutable")
 
-    # primer1 sequence
-    parser.add_argument("-p1", "--read1_primer", help="primer sequence for read one, also refered as prefix in tpp")
+    # # primer1 sequence
+    # parser.add_argument("-p1", "--read1_primer", help="primer sequence for read one, also refered as prefix in tpp")
 
-    # primer2 sequence
-    parser.add_argument("-p2",  "--read2_primer", help="primer sequence for read two, also refered as prefix in tpp")
+    # # primer2 sequence
+    # parser.add_argument("-p2",  "--read2_primer", help="primer sequence for read two, also refered as prefix in tpp")
 
     
-    # protocol
-    parser.add_argument("-prot",  "--protocol", help="protocol; one of Tn5, Sassetti, Mme1; default is Sassetti")
+    # # protocol
+    # parser.add_argument("-prot",  "--protocol", help="protocol; one of Tn5, Sassetti, Mme1; default is Sassetti")
 
 
-    #extract the arguments   
+    # #extract the arguments   
     
 
 
     args = parser.parse_args()
+
+    config_path = args.config_file_path
+
+    tpp_params = get_args(config_path)
+
+    print("\n ********** PROVIDED TPP PARAMS **********\n")
+    for key, value in tpp_params.items():
+        print(f"{key}:\t{value}")
+
     
-    directory_path = args.directory
+    directory_path = tpp_params["raw_data_path"]
 
-    reference = args.reference
+    reference = tpp_params["reference"]
 
-    bwa_path = args.bwa_path
+    bwa_path = tpp_params["bwa_path"]
 
-    primer1 = args.read1_primer
-    primer2 = args.read2_primer
+    primer1 = tpp_params["read1_primer"]
+    primer2 = tpp_params["read2_primer"]
+    protocol = tpp_params["protocol"]
+    replicon_ids = tpp_params["replicon_id"]
 
-    protocol = args.protocol
 
+    # ################################## Call the functions
 
-    ################################## Call the functions
-
-    #
 
     # 2. run tpp
     run_tpp(raw_data_directory=directory_path,
